c0e36d7a4016c4c820b758d38967d83931c2c866
Robin (Ashe) Strife <rstrife@unomaha.edu>
2026-05-29 16:21:52 -0500

    Daily Update v0.112 Dialogue box is harddddddd but ill get it!
    To do: figure out how to implement dialogue.

--- Scripts/Dialogue.cs
+++ Scripts/Dialogue.cs
@@ -9,7 +9,9 @@ public partial class Dialogue : Area2D
 
 	public override void _Ready()
     {
-        _dialogueBox = GetNode<CanvasGroup>("/root/Node2D/Camera2D/DialogueBox");
+    	_dialogueBox = GetNode<CanvasGroup>("/root/Node2D/Camera2D/DialogueBox");
+		//var _dialogic = GetNode("/root/Dialogic");
+		//_dialogic.Call("start", "TimelineName"); 
     }
 	// Called when the node enters the scene tree for the first time.
 	public void OnNPCBodyEntered(PhysicsBody2D body)
@@ -21,6 +23,7 @@ public partial class Dialogue : Area2D
 	public void OnNPCBodyExit(PhysicsBody2D body)
 	{
 		isInside = false;
+		// _dialogueBox.Visible = false;
 	}
 
 	public override void _Input(InputEvent @event)
@@ -38,8 +41,9 @@ public partial class Dialogue : Area2D
 
 	public void DialogueBox()
 	{
-		GD.Print("HELLO IM A DIALOGUE BOX IN TRAINING! IM TRYING MY BEST BUT THIS IS SOOOO HARRRRWWWDDDDDD!");
+		// GD.Print("HELLO IM A DIALOGUE BOX IN TRAINING! IM TRYING MY BEST BUT THIS IS SOOOO HARRRRWWWDDDDDD!");
 		_dialogueBox.Visible = true;
+		//_dialogic.Start("NPCTest");
 	}
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
